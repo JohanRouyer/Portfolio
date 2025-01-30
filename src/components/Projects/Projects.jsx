2ad1e6d1eeea9e7418d56f136432e83a150a6720
Johan ROUYER <johanlolt@gmail.com>
2025-01-30 23:50:12 +0100

    Projects components + data + styles

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,5 +1,18 @@
+import projects from"../../data/projects.json"
+import {ProjectCard} from "./ProjectCard.jsx";
+import styles from "./Project.module.css"
+
 export const Projects = () => {
     return (
-        <section>Projects</section>
+        <section className={styles.container} id="projects">
+            <h2 className={styles.title}>Projects</h2>
+            <div className={styles.projects}>
+                {
+                    projects.map((project,id)=>{
+                      return <ProjectCard key={id} project={project}/>
+                    })
+                }
+            </div>
+        </section>
     )
 }
